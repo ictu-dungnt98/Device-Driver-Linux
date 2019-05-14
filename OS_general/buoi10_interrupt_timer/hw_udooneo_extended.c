531b354084ad914ffda03e04ce8c09cf3f4334ea
trongdung <ntdungembedded98@gmail.com>
2019-05-14 16:42:44 +0700

    fix hw_udooneo_extended.c

--- OS_general/buoi10_interrupt_timer/hw_udooneo_extended.c
+++ OS_general/buoi10_interrupt_timer/hw_udooneo_extended.c
@@ -3,7 +3,7 @@
 #include <linux/printk.h>
 
 int gpio_init(unsigned int *gpio,
-	unsigned int *iomuxc
+	unsigned int *iomuxc,
 	unsigned char port,
 	unsigned char pin, const char *mode)
 {
@@ -13,7 +13,7 @@ int gpio_init(unsigned int *gpio,
 
 	iomuxc += (port + pin);
 	port_add = gpio + port;
-	dir = port_add + GPIO_GDIR;
+	dir_reg = port_add + GPIO_GDIR;
 	gpio += (port + pin);
 
 	/* setting gpio mode */
@@ -44,17 +44,15 @@ int gpio_setPin(unsigned int *gpio,
 	unsigned char pin,
 	unsigned char value)
 {
-	if (!strcmp(mode, "OUTPUT")) {
-		temp = read_reg(gpio, ~(0x01 << pin));
-		temp |= 0x01 << pin;
-		write_reg(gpio, temp);
-		/* set ouput pin to zero */
-		temp = read_reg(dir_reg, ~(0x01 << pin));
-		write_reg(dir_reg, temp);
-	} else {
-		pr_err("gpio pin have not been settup to ouput mode\n");
-		return -1;
-	}
+	unsigned int temp = 0;
+	unsigned int *dir_reg = NULL;
+	unsigned int *port_add = NULL;
+
+	port_add = (gpio + port);
+	dir_reg = port_add + GPIO_GDIR;
+	/* set output pin bt value */
+	temp = read_reg(dir_reg, ~(0x01 << pin));
+	write_reg = (dir_reg, temp);
 	return 0;
 }
 
@@ -62,11 +60,11 @@ char gpio_getPin(unsigned int *gpio,
 	unsigned char port,
 	unsigned char pin)
 {
-	unsigned int char res = 0;
+	unsigned int res = 0;
 	unsigned int *port_add = NULL;
 	unsigned int *gpio_gdir = NULL;
 
-	port_add = gptio + port;
+	port_add = gpio + port;
 	gpio_gdir = port_add + GPIO_GDIR;
 	res = read_reg(gpio_gdir, (0x01 << pin));
 
