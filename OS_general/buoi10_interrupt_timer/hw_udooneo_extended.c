60b5b3adcbc6c9abc57e49c32841179241611e52
trongdung <ntdungembedded98@gmail.com>
2019-05-14 16:14:41 +0700

    fixed warning checkpatch.pl

--- OS_general/buoi10_interrupt_timer/hw_udooneo_extended.c
+++ OS_general/buoi10_interrupt_timer/hw_udooneo_extended.c
@@ -4,33 +4,34 @@
 int gpio_init(unsigned int *gpio,
 	unsigned int *iomuxc
 	unsigned char port,
-	unsigned char pin,const char* mode)
+	unsigned char pin, const char *mode)
 {
 	unsigned int *temp = NULL;
 	unsigned int *dir_reg = NULL;
 	unsigned int *port_add = NULL;
+
 	iomuxc += (port + pin);
 	port_add = gpio + port;
 	dir = port_add + GPIO_GDIR;
 	gpio += (port + pin);
 
 	/* setting gpio mode */
-	temp = read_reg(iomuxc,MUX_MODE_MASK);
+	temp = read_reg(iomuxc, MUX_MODE_MASK);
 	temp |= ALT5;
-	write_reg(iomuxc,temp);
+	write_reg(iomuxc, temp);
 	/* setting input or output mode */
-	if(!strcmp(mode,"INPUT") {
-		temp = read_reg(gpio,~(0x01 << pin));
-		write_reg(gpio,temp);
-	} else if(!strcmp(mode,"OUTPUT") {
-		temp = read_reg(gpio,~(0x01 << pin));
+	if (!strcmp(mode, "INPUT")) {
+		temp = read_reg(gpio, ~(0x01 << pin));
+		write_reg(gpio, temp);
+	} else if (!strcmp(mode, "OUTPUT")) {
+		temp = read_reg(gpio, ~(0x01 << pin));
 		temp |= 0x01 << pin;
-		write_reg(gpio,temp);
+		write_reg(gpio, temp);
 		/* set ouput pin to 1 */
-		temp = read_reg(dir_reg,~(0x01 << pin));
+		temp = read_reg(dir_reg, ~(0x01 << pin));
 		temp |= (0x01 << pin);
-		write_reg(dir_reg,temp);
-	}else {
+		write_reg(dir_reg, temp);
+	} else {
 		pr_err("Invalid argument mode\n");
 		return -1;
 	}
@@ -42,14 +43,14 @@ int gpio_setPin(unsigned int *gpio,
 	unsigned char pin,
 	unsigned char value)
 {
-	if(!strcmp(mode,"OUTPUT") {
-		temp = read_reg(gpio,~(0x01 << pin));
+	if (!strcmp(mode, "OUTPUT")) {
+		temp = read_reg(gpio, ~(0x01 << pin));
 		temp |= 0x01 << pin;
-		write_reg(gpio,temp);
+		write_reg(gpio, temp);
 		/* set ouput pin to zero */
-		temp = read_reg(dir_reg,~(0x01 << pin));
-		write_reg(dir_reg,temp);
-	}else {
+		temp = read_reg(dir_reg, ~(0x01 << pin));
+		write_reg(dir_reg, temp);
+	} else {
 		pr_err("gpio pin have not been settup to ouput mode\n");
 		return -1;
 	}
@@ -62,17 +63,14 @@ char gpio_getPin(unsigned int *gpio,
 {
 	unsigned int char res = 0;
 	unsigned int *port_add = NULL;
-	unsigned int * gpio_gdir = NULL;
+	unsigned int *gpio_gdir = NULL;
 
 	port_add = gptio + port;
 	gpio_gdir = port_add + GPIO_GDIR;
-	res = read_reg(gpio_gdir,(0x01 << pin));
+	res = read_reg(gpio_gdir, (0x01 << pin));
 
-	if(res)
-	{
+	if (res)
 		return 1;
-	}else
-	{
+	else
 		return 0;
-	}
 }
