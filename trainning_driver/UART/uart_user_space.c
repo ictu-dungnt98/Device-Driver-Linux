f4093d162fa1d83245f3982b99381f83bfc7df5f
TrongDungEmb98 <ntdungembedded98@gmail.com>
2021-03-05 21:46:39 +0700

    add userspace-uart driver

---
+++ trainning_driver/UART/uart_user_space.c
@@ -0,0 +1,124 @@
+// Compile : gcc -Wall uart-receive.c -o uart-receive -lwiringPi
+
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+
+int fd;
+
+void clean_stdin(void)
+{
+    int c;
+
+    printf("clean stdin ");
+
+    do {
+        c = getchar();
+    } while (c != '\n' && c != EOF);
+
+    printf("done\n");
+}
+
+static void *thread1_handle(void *arg)
+{
+    int cmd;
+    char command[256];
+
+    while (1) {
+        memset(command, 0, sizeof(command));
+        cmd = 0;
+
+        printf("Select command to send\n");
+        scanf("%d", &cmd);
+
+        clean_stdin();
+
+        switch (cmd) {
+            case 1: {
+                sprintf(command,
+                        "{\"cmd_type\":0, \"brand\":12, \"power\":0, \"temp\":18, \"mode\":1, \"fan\":1, \"swing\":1, "
+                        "\"model\": 4}");
+            } break;
+
+            case 2: {
+                sprintf(command,
+                        "{\"cmd_type\":1, \"brand\":12, \"power\":0, \"temp\":18, \"mode\":1, \"fan\":1, \"swing\":1, "
+                        "\"model\": 4}");
+            } break;
+
+            case 3: {
+                sprintf(command,
+                        "{\"cmd_type\":2, \"brand\":0, \"power\":1, \"temp\":18, \"mode\":1, \"fan\":1, \"swing\":1, "
+                        "\"model\": 4}");
+            } break;
+
+            case 4: {
+                sprintf(command,
+                        "{\"cmd_type\":2, \"brand\":0, \"power\":0, \"temp\":18, \"mode\":1, \"fan\":1, \"swing\":1, "
+                        "\"model\": 4}");
+            } break;
+
+            default:
+                break;
+        }
+
+        write(fd, command, strlen(command));
+        fsync(fd);
+    }
+
+    return 0;
+}
+
+static void *thread2_handle(void *arg)
+{
+    char buff[512];
+
+    int rx_data_len;
+
+    printf("thread2 run\n");
+
+    while (1) {
+        memset(buff, 0, sizeof(buff));
+        rx_data_len = 0;
+
+        rx_data_len = read(fd, buff, 512);
+        
+        if (rx_data_len > 0)
+            printf("%s", buff);
+
+        usleep(10000);
+    }
+
+    printf("thread2 terminate\n");
+
+    return 0;
+}
+
+int main()
+{
+    pthread_t thread1, thread2;
+
+    fd = open("/dev/ttyS0", O_RDWR | O_NOCTTY | O_TRUNC | O_SYNC);
+    if (fd) {
+        printf("Can not open serial port /dev/ttyS0. %s\n", strerror(errno));
+    }
+
+    if (0 != pthread_create(&thread1, NULL, thread1_handle, NULL)) {
+        perror("Can not create thread 1\n");
+        exit(-1);
+    }
+
+    if (0 != pthread_create(&thread2, NULL, thread2_handle, NULL)) {
+        perror("Can not create thread 2\n");
+    }
+
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+
+    return 0;
+}
