450652e1031d584db6f1353bb43dfffb57fe9485
TrongDungEmb98 <ntdungembedded98@gmail.com>
2021-06-10 15:22:16 +0700

    modify shell install vim

--- vim_awesome.sh
+++ vim_awesome.sh
@@ -1,4 +1,3 @@
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
-cat ~/Device-Driver-Linux/vimrc.config > ~/.vimrc
-
+cat ./vimrc.config > ~/.vimrc
