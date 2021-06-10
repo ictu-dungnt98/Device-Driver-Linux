8dd2a062ec2e024c4315673a96f5c3bb3a67a5e5
TrongDungEmb98 <ntdungembedded98@gmail.com>
2021-06-10 15:30:32 +0700

    fixed vim install

--- vim_awesome.sh
+++ vim_awesome.sh
@@ -1,3 +1,4 @@
-git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
+sudo apt-get install -y vim
+git clone --depth=1 https://github.com/amix/vimrc.git ~/vim_runtime
+sh ~/vim_runtime/install_awesome_vimrc.sh
 cat ./vimrc.config > ~/.vimrc
