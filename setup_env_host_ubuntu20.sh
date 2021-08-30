125a30e22945f5c21fa2624692a901f5a18a371b
TrongDungEmb98 <ntdungembedded98@gmail.com>
2021-08-30 14:57:34 +0700

    add shell install package environment

---
+++ setup_env_host_ubuntu20.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt-get install git cmake vim autoconf -y
+sudo apt install python-minimal -y
+sudo apt-get install gawk wget git-core diffstat unzip texinfo gcc-multilib build-essential chrpath socat libsdl1.2-dev xterm -y
+sudo apt-get install libcurl-ocaml-dev libmosquitto-dev libmosquittopp-dev libusb-1.0-0-dev libssl-ocaml-dev -y
+sudo apt-get install bison flex swig libtool curl -y
+sudo apt-get install uuid-dev libacl1-dev liblzo2-dev libzstd-dev lib32z1 device-tree-compiler minicom -y
+sudo apt-get install libncurses5-dev libncursesw5-dev -y
